8e0a4e0eb8e715d5867d5f596a5bbb81eef93f58
nikatnguyen <anguyen11@seattleu.edu>
2023-11-21 16:55:35 -0800

    updated tables

--- module9.sql
+++ module9.sql
@@ -1,9 +1,7 @@
-DROP TABLE employees CASCADE;
-DROP TABLE titles CASCADE;
-DROP TABLE salaries CASCADE;
-DROP TABLE departments CASCADE;
-DROP TABLE dept_managers CASCADE;
-DROP TABLE dept_emp;
+--Use the provided information to create a table schema for each of the six CSV files. 
+--Import each CSV file into its corresponding SQL table.
+
+
 
 CREATE TABLE employees(
 	emp_no SERIAL NOT NULL PRIMARY KEY,
@@ -47,3 +45,23 @@ CREATE TABLE dept_managers(
 	emp_no SERIAL NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
+
+--DATA ANALYSIS--
+
+--List the employee number, last name, first name, sex, and salary of each employee.
+2
+
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+--List each employee in the Sales department, including their employee number, last name, and first name.
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
